74ac858beaae914e9d6ef54a328e10c24702f4e7
bpond37 <bpond37@gmail.com>
2019-11-28 23:41:43 +0900

    first commit

---
+++ src/components/Memo/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Memo () {
+
+  const Memo = styled.div`
+    padding: 4px;
+  `
+
+
+
+  return (
+    <Memo>
+      Memo
+    </Memo>
+  )
+}
+
+export default Memo
---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+import pen from '../../Icon/pen.svg';
+import trashcan from '../../Icon/trashcan.svg';
+
+const Bar = styled.div`
+  display : flex;
+  padding-left : 1rem;
+  background : whitesmoke;
+  height : 40px;
+  border-bottom : 0.3px solid;
+  /* justify-content : center; */
+  /* border-bottom-color :gray; */
+  align-items: center;
+  `
+
+const Button = styled.div`
+  background: white;
+  border-radius : 4px;
+  margin-left : 10px;
+  padding-top : 2px;
+  width : 40px;
+  height : 20px;
+  border : 0.5px solid gray;
+  font-size : 10;
+  /* align-items: center; */
+  justify-content: center;
+  vertical-align: middle;
+  text-align : center;
+  &:hover{
+    background: whitesmoke;
+  }
+`
+
+
+function TopBar (){
+  return(
+    <Bar>
+      <h5>MD Note</h5>
+      <Button>
+        <img src={pen} width={13} height={'auto'} alt={'new memo'} />
+      </Button>
+      <Button>
+        <img src={trashcan} width={13} height={'auto'} alt={'remove memo'} />
+      </Button>
+    </Bar>
+  )
+}
+
+export default TopBar
