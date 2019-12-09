58ae2cfad702792a33d8639277035993fed61d25
bpond37 <bpond37@gmail.com>
2019-12-09 16:19:47 +0900

    refactoring

--- src/components/editor/Editor/index.tsx
+++ src/components/editor/Editor/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, ChangeEvent, useEffect} from 'react'
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
 import styled from 'styled-components'
@@ -14,19 +14,43 @@ type InjectedProps = {
 }
 
 function Editor (props:InjectedProps){
-  const {registerMemo} = props[STORES.MEMO_STORE]
+  const {registerMemo, setMemo, memo} = props[STORES.MEMO_STORE]
   // const initialTime = getDate(now())
-
-  const [text, setText] = useState({editorHtml:''})
+  // const [title, setTitle] = useState('')
+  // const [text, setText] = useState({editorHtml:''})
   const [createdTime, setCreatedTime] = useState(now())
   
-  const handleChange = (html:any) =>{
-    console.log(html)
-    setText({ editorHtml: html });
+  //temporory id
+  let id = 1;
 
+  useEffect(()=>{
+    console.log('useeffect')
+    handleChange('')
+    // setMemo({id:id, title:'', created:createdTime, contents: ''})
+  },[])
+
+  // const initializeEditor = ()=>{
+  //   setText({editorHtml:''})
+  //   setTitle('')
+  //   setCreatedTime(now())
+  // }
+
+  const handleChange = (html:string) =>{
+    // setText({ editorHtml: html });
+    console.log("handlechange")
+    setMemo({...memo, contents:html})
+    setCreatedTime(now())
+  }
+
+  const handleTitle = (e:ChangeEvent<HTMLInputElement>) =>{
+    // setTitle(e.target.value)
+    const tempTitle = e.target.value
+    console.log(tempTitle)
+    setMemo({...memo, title: tempTitle})
     setCreatedTime(now())
 
   }
+  // registerMemo({id:id++, created:createdTime, contents: text.editorHtml})
 
   const modules = {
     toolbar:[
@@ -36,17 +60,21 @@ function Editor (props:InjectedProps){
       ['blockquote', 'code-block', 'link', 'image']
     ],
   }
-  
-
 
   return(
     <EditorDiv>
       <EditorDiv className='createdDate'> {getDateFormat(createdTime)}</EditorDiv>
+      <EditorDiv className='inputTitle'> 
+      <input type='text' value={memo.title} onChange={e=>handleTitle(e)} placeholder='Title'
+       style={{border:'none', fontSize:'1.2rem' }}
+       >
+      </input>
+      </EditorDiv>
       <QuillWrapper>
        <ReactQuill 
           theme='bubble'
           onChange={handleChange}
-          value={text.editorHtml}
+          value={memo.contents}
           modules={modules}
           // placeholder='내용을 작성하세요'
          />
@@ -55,10 +83,15 @@ function Editor (props:InjectedProps){
   )
 }
 
+export default inject(STORES.MEMO_STORE)(observer(Editor))
+
 const EditorDiv = styled.div`
   flex:1;
   display: flex;
   flex-direction:column;
+    /* @media(max-width:768px){
+    flex:1;
+    } */
   &.createdDate{
     height: 2rem;
     flex:0;
@@ -67,24 +100,27 @@ const EditorDiv = styled.div`
     font-size: 0.8rem;
     color: darkgray;
   }
-  &.codeEditor{
-    flex:1;
-    /* background: gray; */
+  &.inputTitle{
+    /* flex: 1; */
+    padding-left : 1rem;
+    font-size: 1rem;
+    height: 1rem;
+    flex:0;
   }
 `
 
 const QuillWrapper = styled.div`
+    padding-top: 1rem;
   .ql-editor{
     flex:1;
-    padding: 0;
     /* height : calc(100vh - 5rem); */
+    padding : 0;
+    padding-left : 1rem;
     min-height: 18em;
-    font-size: 1 rem;
+    font-size: 1rem;
     line-height: 1.5;
   }
   .ql-editor.ql-blank::before{
   left: 0px;
   }
 `
-
-export default inject(STORES.MEMO_STORE)(observer(Editor))
